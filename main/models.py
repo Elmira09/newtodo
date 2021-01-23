4e4240997ad8637ac0e49191025b9f44d5d63986
Your Name <elmira093094@gmail.com git config --global user.name Elmira09>
2021-01-24 03:11:32 +0600

    created author and date in page models

--- main/models.py
+++ main/models.py
@@ -15,8 +15,9 @@ class Books(models.Model):
     description = models.CharField(max_length=300)
     price =  models.CharField(max_length=100)
     genre = models.CharField(max_length=100)
-    year = models.DateField(auto_now=True)
-    published_at = models.DateField(auto_now_add=True)
+    year = models.DateField()
+    author = models.CharField(max_length=100, blank=True)
+    date = models.DateField(auto_now_add=True)
 
 
 
